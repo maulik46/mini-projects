da7aec0cd1a4059898eafd450a09ea72aacd4805
Maulik <maulikpaghdal46@gmail.com>
2022-04-11 22:57:37 +0530

    event countdown project added with swipe delete

--- src/main.js
+++ src/main.js
@@ -3,18 +3,24 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import moment from "moment";
+import Vue3TouchEvents from "vue3-touch-events";
 import "./assets/css/style.css";
 
 
 const app = createApp(App);
   app
   .use(router)
+  .use(Vue3TouchEvents)
   .mount("#app");
 
 app.config.globalProperties.$moment = moment;
 app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$filters = {
-  DDMMYYYY(value) {
+  _DDMMYYYY(value) {
     return moment(value).format('DD-MM-YYYY');
+  },
+  _12HoursTime(date,time) {
+    return moment(`${date} ${time}`).format("hh:mm:ss A")
   }
 }
+
--- src/views/eventCountdown.vue
+++ src/views/eventCountdown.vue
@@ -27,7 +27,7 @@
                     <input
                         type="date"
                         v-model="eventDate"
-                        :min="$moment().add(1,'days').format('YYYY-MM-DD')"
+                        :min="$moment().format('YYYY-MM-DD')"
                         class="h-full w-full px-2 transition-all rounded-sm border"
                         :class="
                             dateError.status ? 'border-red-500' : 'border-gray-300'
@@ -43,9 +43,7 @@
                     <input
                         type="time"
                         v-model="eventTime"
-                        step="5" 
-                         min="02:00"
-                         max="04:00"
+                        step="1" 
                         class="h-full w-full px-2 transition-all rounded-sm border"
                         :class="
                             timeError.status ? 'border-red-500' : 'border-gray-300'
@@ -61,37 +59,45 @@
                 
                 <div class="mt-4">
                     <button
-                        class="py-2.5 px-5 text-center text-white font-semibold bg-green-400 rounded-sm text-sm"
+                        class="py-2.5 px-5 text-center text-white font-semibold bg-green-400 rounded-sm text-sm hover:bg-green-500"
                     >
                         Create
                     </button>
                 </div>
             </form>
         </div>
-        <div class="mt-3 lg:col-span-8 col-span-12 border-2 p-5 rounded-lg bg-white shadow-md">
+        <div class="mt-3 lg:col-span-8 col-span-12 border-2 p-5 rounded-lg bg-white shadow-md overflow-auto" style="max-height:85vh">
             <ul>
-                <li v-for="(event,index) in events" :key="index">
-                    <div class="bg-green-100 shadow-sm border  p-3 w-full rounded-lg mb-2">
-                        <div class="flex justify-between items-center">
-                            <div class="text-3xl">{{event.title}}</div>
-                            <strong class="text-2xl">{{$filters.DDMMYYYY(event.date) }}</strong>
+                <li v-for="(event,index) in events" :key="index" class="relative">
+                    <div class=" shadow-sm border w-full rounded-lg mb-2 eventCard z-30" :class="index === 0 ? 'bg-green-100 p-3' : 'bg-gray-100 py-1.5 px-3' " 
+                    v-touch:swipe.left="handleLeftSwipe()"
+                    v-touch:swipe.right="handleRightSwipe()"
+                    >
+                        <div class="grid grid-cols-4 justify-between items-center">
+                            <div class="select-none">
+                                <div class="text-xl">{{event.title}}</div>
+                                <strong class="text-xs py-0.5 px-2  text-white text-center rounded" :class="event.isFinished ? 'bg-blue-500' : 'bg-green-500' ">{{ event.isFinished ? 'Finished' : 'Upcoming' }}</strong>
+                            </div>
+                            <div class="text-center col-span-2 select-none">
+                                <strong class="" :class="index === 0 ? 'text-3xl' : 'text-xl' ">
+                                    {{ event.leftTime }}
+                                </strong>
+                            </div>
+                            <div class="text-right select-none">
+                                <div :class="index === 0 ? 'text-xl' : 'text-base' ">{{$filters._DDMMYYYY(event.date) }}</div>
+                                <div :class="index === 0 ? 'text-xl' : 'text-base' "> {{$filters._12HoursTime(event.date,event.time)}} </div>
+                            </div>
                         </div>
-                        <div class="flex justify-between items-center">
-                            <div class="text-xs p-0.5 bg-green-500 text-white text-center rounded">Upcoming</div>
-                            <strong class="text-2xl"> {{event.time}} </strong>
-                        </div>
-                        
-                        
-                        
+                    </div>
+                    <div class="eventDelete z-10 absolute right-4" :class="index === 0 ? 'top-6' : 'top-4' ">
+                        <button @click="deleteEvent(event.id)">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-9 w-9 text-red-600 bg-red-100 p-2 rounded-md border border-red-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </button>
                     </div>
                 </li>
-                <li>
-                    <div class="shadow-sm border bg-gray-50 p-4 w-full rounded-lg mb-2 flex justify-between items-center">
-                        <div>event 1 <div class="text-base">09-Apr 2022</div></div>
-                        <strong>12:00:00 <div class="text-sm">Upcoming</div></strong>
-                        
-                    </div>
-                </li>
+                
             </ul>
         </div>
     </div>
@@ -165,13 +171,101 @@ export default {
         },
     },
     methods:{
+        getLeftTime(eventDate,eventTime){
+            const now = this.$moment(new Date()); 
+            const end = this.$moment(`${eventDate} ${eventTime}`); 
+            const duration = this.$moment.duration(end.diff(now));
+
+            
+            const milliSeconds = Math.floor(duration.asMilliseconds());
+            
+            // const months = Math.floor(duration.asMonths());
+            const months = Math.floor(duration/(1000*60*60*24*7*4));
+            const days = Math.floor( duration/(1000*60*60*24) );
+            const hours = Math.floor( (duration/(1000*60*60)) % 24 );
+            const minutes = Math.floor( (duration/1000/60) % 60 );
+            const seconds = Math.floor( (duration/1000) % 60 );
+
+            let leftTime='';
+            let isFinished=false;
+            if(months > 0){
+                leftTime= `${months} ${months > 1 ? ' Months' : 'Month'}  left`;
+            }
+            else if(days > 0){
+                if(days === 1){
+                    leftTime= `${days} Day ${hours} Hours left`;
+                }
+                else{
+                    leftTime= `${days} Days left`;
+                }
+            }
+            else if(hours > 0){
+                if(hours === 1){
+                    leftTime= `${hours} Hour ${minutes} Minutes left`;
+                }
+                else{
+                    leftTime= `${hours} Hours left`;
+                }
+            }
+            else if(minutes > 0){
+                leftTime= `${minutes} Minutes left`;
+            }
+            else if(seconds > 0){
+                leftTime= `${seconds} ${seconds > 1 ? ' Seconds' : 'Second'}  left`;
+            }
+            else{
+                leftTime= 'Event Finished';
+                isFinished=true;
+            }
+            return {
+                duration,
+                milliSeconds,
+                seconds,
+                minutes,
+                hours,
+                days,
+                months,
+                leftTime,
+                isFinished
+            }
+            
+            // console.log({seconds});
+            // console.log({minutes});
+            // console.log({hours});
+            // console.log({days});
+            // console.log({months});
+            // console.log({years});
+        },
+        getLatestTime(){
+            this.events=this.events.map( (item) =>{
+                let getLeftTime= this.getLeftTime(item.date,item.time);
+                return {
+                    ...item,
+                    leftTime: getLeftTime.leftTime,
+                    milliSeconds: getLeftTime.milliSeconds,
+                    isFinished: getLeftTime.isFinished,
+                }
+            })
+            .sort((a,b) => a.milliSeconds - b.milliSeconds)
+            .sort((a,b) => a.isFinished - b.isFinished);
+        },
         eventList(){
             this.$axios
                 .get("http://localhost:3000/eventLists")
                 .then((res) => {
-                    console.log(res);
                     if (res.status === 200) {
-                        this.events=res.data;
+                        let newData=res.data.map( (item) =>{
+                            let getLeftTime= this.getLeftTime(item.date,item.time);
+                            return {
+                                ...item,
+                                leftTime: getLeftTime.leftTime,
+                                milliSeconds: getLeftTime.milliSeconds,
+                                isFinished: getLeftTime.isFinished,
+                            }
+                        })
+                        .sort((a,b) => a.milliSeconds - b.milliSeconds)
+                        .sort((a,b) => a.isFinished - b.isFinished);
+                        this.events=newData;
                     }
                 });
         },
@@ -181,40 +275,82 @@ export default {
                 this.eventDate,
                 this.eventTime,
             );
+            let isValid=true;
             if(this.title == ''){
                 this.titleError.status=true;
                 this.titleError.message="Title is required";
+                isValid=false;
             }
             if(this.eventDate == ''){
                 this.dateError.status=true;
                 this.dateError.message="Date is required";
+                isValid=false;
             }
             if(this.eventTime == ''){
                 this.timeError.status=true;
                 this.timeError.message="Time is required";
+                isValid=false;
             }
-            // return false;
-            // this.$axios
-            //     .post("http://localhost:3000/eventLists/", {
-            //         title: this.title,
-            //         date: this.eventDate,
-            //         time: this.eventTime,
-            //     })
-            //     .then((res) => {
-            //         console.log(res);
-            //         if (res.status === 201) {
-            //         console.log(res);
-                        
-            //         }
-            //     });
+            if(isValid){
+                this.$axios
+                    .post("http://localhost:3000/eventLists/", {
+                        title: this.title,
+                        date: this.eventDate,
+                        time: this.eventTime,
+                    })
+                    .then((res) => {
+                        if (res.status === 201) {
+                            this.eventList();
+                            this.title='';
+                            this.eventDate='';
+                            this.eventTime='';
+                        }
+                    });
+            }
+        },
+        deleteEvent(id){            
+            this.$axios.delete('http://localhost:3000/eventLists/'+id).then((response)=>{
+                if(response.status===200){
+                    this.events=this.events.filter((item) => item.id !== id);
+                    document.querySelectorAll('.eventCard').forEach(item =>{
+                        item.style.right="0px";
+                    });
+                }
+            });
+        },
+        handleLeftSwipe() {
+            return function(direction,ev) {
+                console.log('to left');
+                const eventCard=ev.target.closest(".eventCard");
+                eventCard.style.right="90px";
+            }
+        },
+        handleRightSwipe(){
+            return function(direction,ev) {
+                console.log('to right');
+                const eventCard=ev.target.closest(".eventCard");
+                eventCard.style.right="0px";
+            }
         }
     },
     mounted(){
-        this.eventList()
+        this.eventList();
+        setInterval(()=>{
+             this.getLatestTime();   
+        },1000);
     }
 };
 </script>
 
 <style scoped>
-
+    .eventCard{
+        position: relative;
+        transition: 0.15s ease-in-out;
+        right:0px;
+        cursor: grab;
+    }
+    .eventCard:active{
+        cursor: grabbing;
+        background-color: rgba(167, 243, 208, var(--tw-bg-opacity));
+    }
 </style>
